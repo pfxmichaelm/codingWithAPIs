6e9e37021a4619fa92214ce44b71fe5db4adacf2
Michael Miller <pfxmichaelm@gmail.com>
2015-03-18 10:19:04 -0600

    initial commit

---
+++ apps/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(function() {
+		$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data) {
+			myData = data.Search;
+			$.each(myData, function(index,value) {
+			    console.log(value.Title);
+			});
+		})
+	})
+});
