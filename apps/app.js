d22fb763115bc2b51be4c25a5a0f0a0ffeb24506
Michael Miller <pfxmichaelm@gmail.com>
2015-03-20 11:27:39 -0600

    search finished

--- apps/app.js
+++ apps/app.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
 	$(function() {
-		$.getJSON('http://www.omdbapi.com/?s=Star%20Wars&r=json', function(data) {
-			myData = data.Search;
-			$.each(myData, function(index,value) {
-			    console.log(value.Title);
-			});
-		})
-	})
+		$('#search-term').submit(function(event) {
+			event.preventDefault();
+			var searchTerm = $('#query').val();
+			getRequest(searchTerm);
+		});
+	});
+
+	function getRequest(searchTerm) {
+		var params = {
+			s: searchTerm,
+			r: 'json'
+		};
+		url = 'http://www.omdbapi.com';
+
+        $.getJSON(url, params, function(data) {
+	        showResults(data.Search);
+	    });
+	}
+
+	function showResults(results) {
+		var html = "";
+		$.each(results, function(index,value) {
+			html += '<div>' + value.Title + '<div>';
+			console.log(value.Title);
+		});
+		$('#search-results').html(html);
+	}
 });
